be7d6a6b88d483e8803ca01b364a0a72ef8f2fd1
Simon Goričar <simon.peter.goricar@gmail.com>
2024-02-03 15:43:51 +0100

    chore: set up bare-bones SvelteKit project

---
+++ .eslintrc.cjs
@@ -0,0 +1,49 @@
+/** @type { import("eslint").Linter.Config } */
+module.exports = {
+	root: true,
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:svelte/recommended"
+	],
+	parser: "@typescript-eslint/parser",
+	plugins: [
+		"@typescript-eslint"
+	],
+	parserOptions: {
+		sourceType: "module",
+		ecmaVersion: 2020,
+		extraFileExtensions: [
+			".svelte"
+		]
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	},
+	rules: {
+		"indent": "off",
+		"@typescript-eslint/indent": ["error", 4],
+
+		"no-multiple-empty-lines": [
+			"warn",
+			{
+				"max": 4,
+				"maxEOF": 1,
+			}
+		],
+
+		"quotes": "off",
+		"@typescript-eslint/quotes": ["warn", "double"],
+	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	]
+};
