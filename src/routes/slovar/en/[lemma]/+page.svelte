e8f7c64d34801dbc4f5b13cecd4594a39eb8d4cd
Simon Goričar <simon.peter.goricar@gmail.com>
2025-01-06 22:00:19 +0100

    Continue work on frontend

--- src/routes/slovar/en/[lemma]/+page.svelte
+++ src/routes/slovar/en/[lemma]/+page.svelte
@@ -7,7 +7,7 @@
 </script>
 
 
-<div class="km-container km-centering-container">
+<!--<div class="km-container km-centering-container">
     <div class="o-central-column km-elevated">
         <div class="c-heading">
             <h3 class="c-heading__lemma">{ word.lemma }</h3>
@@ -15,7 +15,22 @@
         </div>
         <p>{ word.description }</p>
     </div>
-</div>
+</div>-->
 
+
+<div class="km_dictionary-en">
+    <div class="km_dictionary-en_container">
+        <div class="km_word-en">
+            <div class="km_word-en_lemma">
+                {word.lemma}
+            </div>
+        
+            <div class="km_word-en_description">
+                {word.description}
+            </div>
+        </div>
+    </div>
+</div>
+<!-- 
 <p>{ data.lemma }</p>
-<p>{ JSON.stringify(data.wordInfo) }</p>
+<p>{ JSON.stringify(data.wordInfo) }</p> -->
